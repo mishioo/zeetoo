d09448c3e9a92e0fdb52a0a9aab0220416717b83
Michał Więcław <wieclawmm@gmail.com>
2019-06-17 20:30:54 +0200

    Made possible writting simultaneously to file and stdout.

--- gofproc.py
+++ gofproc.py
@@ -3,6 +3,7 @@ import re
 import argparse
 from itertools import chain
 from pathlib import Path
+import logging as lgg
 
 number_group = r'\s*(-?\d+\.?\d*)'
 number = number_group.replace('(', '').replace(')', '')
@@ -16,55 +17,8 @@ frequencies = re.compile(
     'Frequencies(?:\s+(?:Fr= \d+)?--\s+)' + 3 * number_group
 )
 
-def get_data(path):
-    with path.open('r') as file:
-        text = file.read()
-    ens = energies.search(text)
-    freqs = frequencies.findall(text)
-    if not ens or not freqs:
-        print(f'{path.name} - NOT CONVERGED')
-        return []
-    else:
-        imag = sum(float(f) < 0 for line in freqs for f in line)
-        return [path.name, *ens.groups(), imag]
 
-
-def main(excel, files):
-    data = []
-    for path in files:
-        line = get_data(path)
-        if line:
-            data.append(line)
-    print(f"Got {len(data)} files to show")
-    if not excel:
-        try:
-            length = max(len(line[0]) for line in data)
-        except ValueError:
-            length = 0
-        for file, zpe, ten, ent, gib, imag in data:
-            text = f"{file: <{length}} - "\
-                   f"ZPE= {zpe: >12} "\
-                   f"TEN= {ten: >12} "\
-                   f"ENT= {ent: >12} "\
-                   f"GIB= {gib: >12} "\
-                   f"Imag.Freqs= {imag: >2} "
-            print(text)
-    else:
-        wb = opxl.load_workbook(str(excel))
-        sheet = wb.active
-        for file, zpe, ten, ent, gib, imag in data:
-            sheet.append([
-                file, '', #  place for comment on this file
-                float(zpe),
-                float(ten),
-                float(ent),
-                float(gib),
-                imag
-            ])
-        wb.save(str(excel))
-
-
-if __name__ == '__main__':
+def get_args(argv=None):
     prs = argparse.ArgumentParser(
         description='Process gaussian optimization files to get energies and '
                     'number of imaginary frequencies.'
@@ -78,7 +32,29 @@ if __name__ == '__main__':
         help='path to excel file; if not specified output '
              'will be printed to stdout'
     )
-    args = prs.parse_args()
+    prs.add_argument(
+        '-s', '--silent', action='store_true',
+        help='Suppress printing to sdtout'
+    )
+    return prs.parse_args(argv)
+
+
+def get_data(path):
+    with path.open('r') as file:
+        text = file.read()
+    ens = energies.search(text)
+    freqs = frequencies.findall(text)
+    if not ens or not freqs:
+        lgg.info(f'{path.name} - NOT CONVERGED')
+        return []
+    else:
+        imag = sum(float(f) < 0 for line in freqs for f in line)
+        return [path.name, *ens.groups(), imag]
+
+
+def main():
+    args = get_args()
+    lgg.basicConfig(level='WARNING' if args.silent else 'INFO')
     dirs = (path for path in args.files if path.is_dir())
     inner_files = (
         path for dir in dirs for path in dir.iterdir()
@@ -88,4 +64,42 @@ if __name__ == '__main__':
         path for path in args.files
         if path.is_file() and path.suffix in ['.log', '.out']
     )
-    main(args.excel, chain(args_files, inner_files))
+    files = chain(args_files, inner_files)
+    data = []
+    for path in files:
+        line = get_data(path)
+        if line:
+            data.append(line)
+    lgg.info(f"Got {len(data)} files to show")
+    try:
+        length = max(len(line[0]) for line in data)
+    except ValueError:
+        length = 0
+    if args.excel:
+        wb = opxl.load_workbook(str(args.excel))
+        sheet = wb.active
+    for file, zpe, ten, ent, gib, imag in data:
+        lgg.info(
+            f"{file: <{length}} - "
+            f"ZPE= {zpe: >12} "
+            f"TEN= {ten: >12} "
+            f"ENT= {ent: >12} "
+            f"GIB= {gib: >12} "
+            f"Imag.Freqs= {imag: >2} "
+        )
+        if args.excel:
+            sheet.append([
+                file, '', #  place for comment on this file
+                float(zpe),
+                float(ten),
+                float(ent),
+                float(gib),
+                imag
+            ])
+    if args.excel:
+        wb.save(str(args.excel))
+
+
+if __name__ == '__main__':
+
+    main()
