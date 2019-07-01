b04e14e0b5197befedd29c2d02053de760f740c8
Michał Więcław <wieclawmm@gmail.com>
2019-07-02 00:44:05 +0200

    Added appending functionality and minor tweeks.

--- zeetoo/gofproc.py
+++ zeetoo/gofproc.py
@@ -36,10 +36,6 @@ def get_args(argv=None):
         help='Wries output to specified excel file.'
     )
     prs.add_argument(
-        '-s', '--silent', action='store_true',
-        help='Suppress printing to sdtout.'
-    )
-    prs.add_argument(
         '-c', '--corrections', action='store_true',
         help='Include corrections in the output.'
     )
@@ -51,7 +47,25 @@ def get_args(argv=None):
         '-e', '--energies', action='store_true',
         help='Include energies values in the output.'
     )
-    return prs.parse_args(argv)
+    prs.add_argument(
+        '-a', '--append_entry', metavar='COL', type=int,
+        help='Append output values to entry, if it already exists, otherwise '
+             'add a new entry. Values will be written to sheet starting at '
+             'column COL.'
+    )
+    log = prs.add_mutually_exclusive_group()
+    log.add_argument(
+        '-u', '--unconverged', action='store_true',
+        help='Print names of files that did not converged.'
+    )
+    log.add_argument(
+        '-s', '--silent', action='store_true',
+        help='Suppress printing to sdtout.'
+    )
+    args = prs.parse_args(argv)
+    if args.append_entry is not None and args.file is None:
+        prs.error("--append_entry needs --file to be specified.")
+    return args
 
 
 def get_data(path):
@@ -60,11 +74,32 @@ def get_data(path):
     ens = energies.search(text)
     freqs = frequencies.findall(text)
     if not ens or not freqs:
-        lgg.info(f'NOT CONVERGED: {path.name}')
+        lgg.debug(f'NOT CONVERGED: {path.name}')
         return []
     else:
         imag = sum(float(f) < 0 for line in freqs for f in line)
-        return [path.name, *ens.groups(), imag]
+        return [*ens.groups(), imag]
+
+
+def select_data(line, args):
+    entries = []
+    if not line:
+        return entries
+    zcr, tcr, ecr, gcr, zpe, ten, ent, gib, imag = line
+    # entry: name, value
+    if args.energies:
+        entries.append(("ZPE", float(zpe)))
+        entries.append(("TEN", float(ten)))
+        entries.append(("ENT", float(ent)))
+        entries.append(("GIB", float(gib)))
+    if args.corrections:
+        entries.append(("ZPECORR", float(zcr)))
+        entries.append(("TENCORR", float(tcr)))
+        entries.append(("ENTCORR", float(ecr)))
+        entries.append(("GIBCORR", float(gcr)))
+    if args.imag_count:
+        entries.append(("Imag.Freqs", int(imag)))
+    return entries
 
 
 def main():
@@ -80,46 +115,45 @@ def main():
         if path.is_file() and path.suffix in ['.log', '.out']
     )
     files = chain(args_files, inner_files)
-    data = []
-    for path in files:
-        line = get_data(path)
-        if line:
-            data.append(line)
-    lgg.info(f"Got {len(data)} files to show")
-    try:
-        length = max(len(line[0]) for line in data)
-    except ValueError:
-        length = 0
+    lines = ((path.name, get_data(path)) for path in files)
+    data = ((file, select_data(line, args)) for file, line in lines)
     if args.file:
         wb = opxl.load_workbook(str(args.file))
         sheet = wb.active
-    for file, zcr, tcr, ecr, gcr, zpe, ten, ent, gib, imag in data:
-        entries = []
-        # entry: name, value, width
-        if args.energies:
-            entries.append(("ZPE", float(zpe)))
-            entries.append(("TEN", float(ten)))
-            entries.append(("ENT", float(ent)))
-            entries.append(("GIB", float(gib)))
-        if args.corrections:
-            entries.append(("ZPECORR", float(zcr)))
-            entries.append(("TENCORR", float(tcr)))
-            entries.append(("ENTCORR", float(ecr)))
-            entries.append(("GIBCORR", float(gcr)))
-        if args.imag_count:
-            entries.append(("Imag.Freqs", int(imag)))
-        if entries:
-            lgg.info(
-                ' '.join((
-                    f"{file: <{length}} -",
-                    *("{}= {}".format(*e) for e in entries)
-                ))
-            )
-        if entries and args.file:
+        owned = {row[0].value: row[0].row for row in sheet.iter_rows(max_col=1)}
+    else:
+        wb, sheet, owned = None, None, {}
+    unconverged = []
+    length = 0
+    for file, entries in data:
+        if not entries:  # no data extracted
+            unconverged.append(file)
+            continue
+        length = len(file) if len(file) > length else length
+        lgg.info(
+            ' '.join((
+                f"{file: <{length}} -",
+                *("{}= {}".format(*e) for e in entries)
+            ))
+        )
+        if args.file and args.append_entry is not None:
+            if file in owned:
+                row = owned[file]
+            else:
+                row = sheet.max_row + 1
+                sheet.cell(row=row, column=1, value=file)
+            col = args.append_entry
+            for n, value in enumerate(entries):
+                sheet.cell(row=row, column=col+n, value=value[1])
+        elif args.file:
             sheet.append([
                 file, '',  # place for comment on this file
                 *(e[1] for e in entries)
             ])
+    if args.unconverged:
+        lgg.info(f"{len(unconverged)} unconverged files:")
+        for filename in unconverged:
+            lgg.info(f'\t{filename}')
     if args.file:
         wb.save(str(args.file))
 
