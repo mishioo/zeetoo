163b47efe91bebb01d15793655958a236b43a635
Michal M. Wieclaw <wieclawmm@gmail.com>
2020-04-20 20:38:23 +0200

    Supplemented type annotations. Reformatted code (black).

--- zeetoo/sdf_to_gjf.py
+++ zeetoo/sdf_to_gjf.py
@@ -2,74 +2,84 @@ import argparse
 import logging as lgg
 import re
 import pathlib
+from typing import Tuple, Generator, Optional, TextIO
 
-
-coords = re.compile(r'\s*(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s*(\w\w?)')
+coords = re.compile(r"\s*(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s*(\w\w?)")
 
 
 def get_parser(argv=None):
-    prsr = argparse.ArgumentParser(
-        description="Generate .gjf files from .sdf file."
-    )
-    prsr.add_argument('sdf', type=pathlib.Path, help="Source .sdf file.")
+    prsr = argparse.ArgumentParser(description="Generate .gjf files from .sdf file.")
+    prsr.add_argument("sdf", type=pathlib.Path, help="Source .sdf file.")
     prsr.add_argument(
-        '-o', '--out_dir', type=pathlib.Path,
+        "-o",
+        "--out_dir",
+        type=pathlib.Path,
         help=(
             "Path to output directory. If not given, evaluates to directory "
             "that given .sdf file resides in."
-        )
+        ),
     )
     prsr.add_argument(
-        '-r', '--route', required=True,
+        "-r",
+        "--route",
+        required=True,
         help=(
             "Calculations specification. If given string doesn't start with "
             "'#' (pound symbol), it is prefixed with '# ' (pound symbol and space)."
-        )
+        ),
     )
     prsr.add_argument(
-        '-d', '--dscr', default='No additional information given.',
+        "-d",
+        "--dscr",
+        default="No additional information given.",
         help=(
             "A comment with brief description of the calculation job. "
             "Defaults to 'No additional information given.'."
-        )
+        ),
     )
+    prsr.add_argument("-l", "--link", default="", help="List of link-0 commands.")
     prsr.add_argument(
-        '-l', '--link', default='', help="List of link-0 commands."
-    )
-    prsr.add_argument(
-        '-s', '--suffix', default='',
+        "-s",
+        "--suffix",
+        default="",
         help=(
             "Everything after molecule specification. This part will be written "
             "as-is, so it should use '\\n' as a new line character if suffix will "
             "span multiple lines. It is an empty string by default."
-        )
+        ),
     )
     prsr.add_argument(
-        '-c', '--charge', default=0, type=int,
-        help="Molecule's charge, defaults to 0."
+        "-c", "--charge", default=0, type=int, help="Molecule's charge, defaults to 0."
     )
     prsr.add_argument(
-        '-m', '--multiplicity', default=1, type=int,
-        help="Molecule's spin multiplicity, defaults to 1."
+        "-m",
+        "--multiplicity",
+        default=1,
+        type=int,
+        help="Molecule's spin multiplicity, defaults to 1.",
     )
     prsr.add_argument(
-        '-n', '--name',
+        "-n",
+        "--name",
         help=(
             'Base name of the output files, it will be appended with "[number].gjf". '
-            'Defaults to base name of the .sdf file given.'
-        )
+            "Defaults to base name of the .sdf file given."
+        ),
     )
     prsr.add_argument(
-        '-f', '--first_num', type=int, default=0,
+        "-f",
+        "--first_num",
+        type=int,
+        default=0,
         help=(
-            'Number of the first conformer, defaults to 0. Numeration of resulting '
-            'files will start at this number.'
-        )
+            "Number of the first conformer, defaults to 0. Numeration of resulting "
+            "files will start at this number."
+        ),
     )
     return prsr
-    
-    
-def get_coords(file, line):
+
+
+def get_coords(file: TextIO, line: str) -> Tuple[str, float, float, float]:
     match = coords.search(line)
     while match is not None:
         x, y, z, a = match.groups()
@@ -77,47 +87,51 @@ def get_coords(file, line):
         match = coords.search(file.readline())
 
 
-def get_molecules(source: pathlib.Path):
+def get_molecules(source: pathlib.Path) -> Generator[Tuple[str, float, float, float]]:
     with source.open() as sdf:
         for line in sdf:
             if coords.search(line) is not None:
                 yield get_coords(sdf, line)
-        
-        
+
+
 def save_molecule(
-    dest: pathlib.Path, charge: int, multipl: int, coords: list, command: str,
-    comment: str = '', prefix: str = '', suffix: str = ''
-):
-    with dest.open('w') as gjf:
+    dest: pathlib.Path,
+    charge: int,
+    multipl: int,
+    coords: Tuple[str, float, float, float],
+    command: str,
+    comment: str = "",
+    prefix: str = "",
+    suffix: str = "",
+) -> None:
+    with dest.open("w") as gjf:
         if prefix:
             gjf.write(prefix)
-            gjf.write('\n')
-        if not command.startswith('#'):
-            command = '# ' + command
+            gjf.write("\n")
+        if not command.startswith("#"):
+            command = "# " + command
         gjf.write(command)
-        gjf.write('\n\n')
+        gjf.write("\n\n")
         if comment:
             gjf.write(comment)
-            gjf.write('\n\n')
-        gjf.write(f'{charge} {multipl}\n')
+            gjf.write("\n\n")
+        gjf.write(f"{charge} {multipl}\n")
         for a, x, y, z in coords:
-            gjf.write(
-                f" {a: <2} {x: > .7f} {y: > .7f} {z: > .7f}\n"
-            )
+            gjf.write(f" {a: <2} {x: > .7f} {y: > .7f} {z: > .7f}\n")
         if suffix:
-            gjf.write('\n')
+            gjf.write("\n")
             gjf.write(suffix)
-        gjf.write('\n\n')
-        
+        gjf.write("\n\n")
 
-def main(argv: list = None):
+
+def main(argv: Optional[list] = None) -> None:
     args = get_parser().parse_args(argv)
     gjfname = args.name if args.name is not None else args.sdf.stem
     out_dir = args.out_dir if args.out_dir is not None else args.sdf.parent
     out_dir.mkdir(exist_ok=True)
     start = args.first_num
     for num, mol in enumerate(get_molecules(args.sdf)):
-        output_file = out_dir / f'{gjfname}{num+start:0>3}.gjf'
+        output_file = out_dir / f"{gjfname}{num+start:0>3}.gjf"
         save_molecule(
             dest=output_file,
             charge=args.charge,
@@ -130,6 +144,6 @@ def main(argv: list = None):
         )
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     main()
