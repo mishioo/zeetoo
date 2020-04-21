e836281b6d2509a6453e8acd6097c1d8062e1dbe
Michał Więcław <wieclawmm@gmail.com>
2020-04-21 11:34:19 +0200

    Refactored tests to use pytest.

--- test/fixgvmol_test.py
+++ test/fixgvmol_test.py
@@ -1,12 +1,28 @@
-import unittest
+import pytest
 from zeetoo.fixgvmol import fix_cont
 
 
-class FixContTest(unittest.TestCase):
+@pytest.fixture
+def expected():
+    return """
 
-    def test_missing_both(self):
-        self.assertSequenceEqual(
-            fix_cont("""
+Created by GaussView 5.0.8
+  5  4  0  0  0  0  0  0  0  0  0 V2000
+    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
+    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
+    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  1  0  0  0  0
+  1  3  1  0  0  0  0
+  1  4  1  0  0  0  0
+  1  5  1  0  0  0  0
+M END
+"""
+
+
+def test_missing_both(expected):
+    cont = """
 
 Created by GaussView 5.0.8
   5  4  0  0  0  0  0  0  0  0  0    0
@@ -19,27 +35,12 @@ Created by GaussView 5.0.8
   1  3  1  0  0  0  0
   1  4  1  0  0  0  0
   1  5  1  0  0  0  0
-"""),
-            """
-
-Created by GaussView 5.0.8
-  5  4  0  0  0  0  0  0  0  0  0 V2000
-    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  3  1  0  0  0  0
-  1  4  1  0  0  0  0
-  1  5  1  0  0  0  0
-M END
 """
-        )
+    assert fix_cont(cont) == expected
 
-    def test_missing_version(self):
-        self.assertSequenceEqual(
-            fix_cont("""
+
+def test_missing_version(expected):
+    cont = """
 
 Created by GaussView 5.0.8
   5  4  0  0  0  0  0  0  0  0  0    0
@@ -53,41 +54,12 @@ Created by GaussView 5.0.8
   1  4  1  0  0  0  0
   1  5  1  0  0  0  0
 M END
-"""),
-            """
-
-Created by GaussView 5.0.8
-  5  4  0  0  0  0  0  0  0  0  0 V2000
-    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  3  1  0  0  0  0
-  1  4  1  0  0  0  0
-  1  5  1  0  0  0  0
-M END
 """
-        )
+    assert fix_cont(cont) == expected
 
-    def test_missing_end(self):
-        self.assertSequenceEqual(
-            fix_cont("""
 
-Created by GaussView 5.0.8
-  5  4  0  0  0  0  0  0  0  0  0 V2000
-    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  3  1  0  0  0  0
-  1  4  1  0  0  0  0
-  1  5  1  0  0  0  0
-"""),
-            """
+def test_missing_end(expected):
+    cont = """
 
 Created by GaussView 5.0.8
   5  4  0  0  0  0  0  0  0  0  0 V2000
@@ -100,41 +72,9 @@ Created by GaussView 5.0.8
   1  3  1  0  0  0  0
   1  4  1  0  0  0  0
   1  5  1  0  0  0  0
-M END
 """
-        )
+    assert fix_cont(cont) == expected
 
-    def test_input_ok(self):
-        self.assertSequenceEqual(
-            fix_cont("""
 
-Created by GaussView 5.0.8
-  5  4  0  0  0  0  0  0  0  0  0 V2000
-    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  3  1  0  0  0  0
-  1  4  1  0  0  0  0
-  1  5  1  0  0  0  0
-M END
-"""),
-            """
-
-Created by GaussView 5.0.8
-  5  4  0  0  0  0  0  0  0  0  0 V2000
-    0.7531    0.9467    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098   -0.0622    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511    0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-    1.1098    1.4511   -0.8737 H   0  0  0  0  0  0  0  0  0  0  0  0
-   -0.3169    0.9467    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  3  1  0  0  0  0
-  1  4  1  0  0  0  0
-  1  5  1  0  0  0  0
-M END
-"""
-        )
-
+def test_input_ok(expected):
+    assert fix_cont(expected) == expected
